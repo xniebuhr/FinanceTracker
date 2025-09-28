335b34de7b457280cab5cac1c917d143f3a8f15f
xniebuhr <xniebuhr@gmail.com>
2025-09-28 15:31:04 -0500

    Delete start_project.bat

--- start_project.bat
+++
@@ -1,7 +0,0 @@
-cd /d "finance_tracker.backend\finance_tracker.backend"
-start cmd /k dotnet run --launch-profile "https"
-
-cd /d "..\finance_tracker.frontend"
-start cmd /k ng serve
-
-start "" "http://localhost:4200/"
